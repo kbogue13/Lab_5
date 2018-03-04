fc438ca16399dee8d362b709dea836f73b93ac25
kbogue13 <kbogue13@uw.edu>
2018-03-04 10:51:45 -0800

    Upload XCode 8.2.1 SWIFT 3 Simple Map

---
+++ Lab5_XCode.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Lab 5
+//
+//  Created by Kevin Bogue on 3/4/18.
+//  Copyright © 2018 Kevin Bogue. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    let manager = CLLocationManager()
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations[0]
+        
+        let span = MKCoordinateSpanMake(0.03, 0.03)
+        let myLocation = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude)
+        
+        let region = MKCoordinateRegionMake(myLocation, span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        self.mapView.showsUserLocation = true
+        
+        statusLabel.text = "\(location.speed)"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
